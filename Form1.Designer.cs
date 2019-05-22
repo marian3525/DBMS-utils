ded221148acf637b4c5c6160cc6ae18fd9e20bd3
Risca Marian <marian.risca1998@outlook.com>
2019-05-22 17:58:20 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace DBMS_Practical_Exam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridParent = new System.Windows.Forms.DataGridView();
+            this.dataGridChild = new System.Windows.Forms.DataGridView();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridParent)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridChild)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridParent
+            // 
+            this.dataGridParent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridParent.Location = new System.Drawing.Point(12, 12);
+            this.dataGridParent.Name = "dataGridParent";
+            this.dataGridParent.RowHeadersWidth = 47;
+            this.dataGridParent.Size = new System.Drawing.Size(242, 305);
+            this.dataGridParent.TabIndex = 0;
+            // 
+            // dataGridChild
+            // 
+            this.dataGridChild.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridChild.Location = new System.Drawing.Point(260, 12);
+            this.dataGridChild.Name = "dataGridChild";
+            this.dataGridChild.RowHeadersWidth = 47;
+            this.dataGridChild.Size = new System.Drawing.Size(239, 305);
+            this.dataGridChild.TabIndex = 1;
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(13, 324);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(92, 43);
+            this.connectButton.TabIndex = 2;
+            this.connectButton.Text = "Connect to DB";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(520, 324);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 35);
+            this.addButton.TabIndex = 3;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(601, 324);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 35);
+            this.updateButton.TabIndex = 4;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(682, 324);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(75, 35);
+            this.removeButton.TabIndex = 5;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(636, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(636, 67);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(156, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(636, 107);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(156, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(517, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 15);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(517, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 15);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(517, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 15);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.dataGridChild);
+            this.Controls.Add(this.dataGridParent);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridParent)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridChild)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridParent;
+        private System.Windows.Forms.DataGridView dataGridChild;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBMS_Practical_Exam
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection = null;
+        DataSet dataSet = null;
+        SqlDataAdapter childAdapter = null;
+        SqlDataAdapter parentAdapter = null;
+        SqlCommandBuilder commandBuilder = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            connection = new SqlConnection(@"Data Source=DESKTOP-9LEJM0T\SQLEXPRESS; Initial Catalog=DBName; Integrated Security=true");
+            dataSet = new DataSet();
+
+            childAdapter = new SqlDataAdapter("select * from ChildTable", connection);
+            parentAdapter = new SqlDataAdapter("select * from ParentTable", connection);
+            commandBuilder = new SqlCommandBuilder(childAdapter);
+
+            parentAdapter.Fill(dataSet, "ParentTable");
+            childAdapter.Fill(dataSet, "ChildTable");
+
+            DataRelation dr = new DataRelation("FK_Parent_Child", dataSet.Tables["ParentTable"].Columns["id"], dataSet.Tables["ChildTable"].Columns["id"]);
+            dataSet.Relations.Add(dr);
+
+            BindingSource bsParent = new BindingSource();
+            BindingSource bsChild = new BindingSource();
+
+            bsParent.DataSource = dataSet;
+            bsParent.DataMember = "ParentTable";
+
+            bsChild.DataSource = bsParent;
+            bsChild.DataMember = "FK_Parent_Child";
+
+            dataGridChild.DataSource = bsChild;
+            dataGridParent.DataSource = bsParent;
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            childAdapter.Update(dataSet, "ChildTable");
+        }
+    }
+}
